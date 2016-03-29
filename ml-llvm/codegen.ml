52be1adf93be773a65a6e0bbb69c223ce3e38737
kdj2109 <kdj2109@columbia.edu>
2016-03-28 20:36:49 -0400

    commit

--- ml-llvm/codegen.ml
+++ ml-llvm/codegen.ml
@@ -113,7 +113,7 @@ let translate (globals, functions) =
           | A.Not     -> L.build_not) e' "tmp" builder
       | A.Assign (s, e) -> let e' = expr builder e in
 	                   ignore (L.build_store e' (lookup s) builder); e'
-      | A.Call ("print", [e]) | A.Call ("printb", [e]) | A.Call ("printf", [e]) ->
+      | A.Call ("print", [e]) | A.Call ("printb", [e]) ->
 	  L.build_call printf_func [| int_format_str ; (expr builder e) |]
 	    "printf" builder
       | A.Call (f, act) ->
