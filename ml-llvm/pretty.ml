3069c1f1de62eb831cee79d3bd05a26fe4f220f1
kdj2109 <kdj2109@columbia.edu>
2016-03-29 15:59:22 -0400

    pretty printing ast

--- ml-llvm/pretty.ml
+++ ml-llvm/pretty.ml
@@ -1,11 +1,7 @@
 open Ast
-open Parser
-
-let rec pretty_print = function 
-    Literal(l) -> string_of_int l
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  let expr = Parser.expr Scanner.token lexbuf in
-  let result = pretty_print expr in
-  print_endline result
+  let program = Parser.program Scanner.token lexbuf in
+  print_endline (string_of_program program)
+
