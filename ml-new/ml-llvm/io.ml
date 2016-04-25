fafa9d9899033c5a397511007bc50636c49aa3c4
Jessica Valarezo <jgv2108@columbia.edu>
2016-04-24 21:24:07 -0400

    I/O compiles

--- ml-new/ml-llvm/io.ml
+++ ml-new/ml-llvm/io.ml
@@ -1,16 +1,15 @@
+
 let filename = "tests/io.mxl" in
-let read_file filename = 
+(* let find_open l = "hello" in *)
 let lines = ref [] in
 let chan = open_in filename in
 try
   while true; do
-    lines := input_line chan :: !lines
-  done; !lines
+    let line = input_line chan in
+
+    print_endline line;
+    lines := line :: !lines;
+  done; []
 with End_of_file ->
   close_in chan;
-  List.rev !lines 
-;;  
-  
-(*let rec print_list = function *)
-(*[] -> ()*)
-(*| e::1 print_string e; print_string " "; print_list lines*)  
+  List.rev !lines
