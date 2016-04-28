ba8d549207ddec48fa6d342ef7104cd2a212c230
Jessica Valarezo <jgv2108@columbia.edu>
2016-04-28 15:55:17 -0400

    Reads in ppm and creates matrix declaration string.

--- ml-new/ml-llvm/io.ml
+++ ml-new/ml-llvm/io.ml
@@ -1,6 +1,6 @@
 let rec print_list = function
   [] -> ()
-| e::l -> print_string e ; print_string "," ; print_list l
+| e::l -> print_string e ; print_string " " ; print_list l
 in
 let is_decimal e = 
   try ignore(Str.search_forward (Str.regexp "[0-9]+") e 0); true
@@ -8,33 +8,31 @@ let is_decimal e =
 in
 let clean_string l =
   let only_digits = List.find_all is_decimal (Str.split (Str.regexp " ") l ) in
-  (String.concat ","
-    (List.map (fun i ->  i) only_digits))
+  (String.concat "," (List.mapi (fun i x -> if (i + 1) mod 3 = 0 then (x ^ ")")
+                                            else if i mod 3 = 0 then ("(" ^ x) else x) only_digits)) 
 in
 let read_ppm ppmname =
-  let filename = ppmname in
-  let ppm_chan = open_in filename in
+  let ppm_chan = open_in ppmname in
+  let w = ref "" in
+  let h = ref "" in 
   let ppm_lines = ref [] in
   try
     let first_line = input_line ppm_chan in
-    let dimensions = Str.split (Str.regexp " ") (input_line ppm_chan) in
+    let d = Str.split (Str.regexp " ") (input_line ppm_chan) in
+    w := List.nth d 0;
+    h := List.nth d 1;
     let max = input_line ppm_chan in
-    print_endline first_line; print_endline max; print_list dimensions;
+    print_endline first_line; print_endline max; print_list d;
     print_string "\n";
     while true; do
-      let ppm_line = input_line ppm_chan in
-      let clean_line = clean_string ppm_line in 
-      ppm_lines := clean_line :: !ppm_lines;
-      (*print_string clean_line;*)
+      ppm_lines := (clean_string (input_line ppm_chan)) :: !ppm_lines;
     done;
-  with End_of_file ->
-    close_in ppm_chan;
-    let all_digits = (String.concat "," (List.map (fun i -> i) !ppm_lines)) in
-    print_endline all_digits;
+    "|" ^ (String.concat "," (List.map (fun i -> i) !ppm_lines)) ^ "|" ^ !w ^ "," ^ !h;
+  with End_of_file -> ignore(close_in ppm_chan); "|" ^ (String.concat "," (List.map (fun i -> i) !ppm_lines)) ^ "|" ^ !w ^ "," ^ !h;
 in
+let open_regex = Str.regexp "open\\(.+*\\)" in 
 let has_open l = 
-  let r = Str.regexp "open\\(.+*\\)" in
-  try ignore (Str.search_forward r l 0); true
+  try ignore (Str.search_forward (open_regex) l 0); true
   with Not_found -> false
 in
 let filename = "file/io.mxl" in
@@ -44,10 +42,9 @@ let chan = open_in filename in
 try
   while true; do
     let line = input_line chan in
-    lines := line :: !lines;
-    if (has_open line) then read_ppm "file/pic.ppm";
+    lines := (if has_open line then Str.replace_first open_regex (read_ppm "file/pic.ppm") line else line) :: !lines;
   done; []
 with End_of_file ->
   close_in chan;
-  List.rev !lines
+  List.rev !lines;
 in read_file filename
