418b442cb6d3bd415ae312a0cb0f82181a3a4c6f
Jessica Valarezo <jgv2108@columbia.edu>
2016-05-07 01:21:19 -0400

    Proper formatting according to new matrix declaration.

--- ml-new/ml-llvm/prep.ml
+++ ml-new/ml-llvm/prep.ml
@@ -36,36 +36,47 @@ let process filename =
         (String.concat "," (List.mapi (fun i x -> if (i + 1) mod 3 = 0 then (x ^ ")")
                                               else if i mod 3 = 0 then ("(" ^ x) else x) only_digits)) 
       in
-      let ppm_chan = open_in ppmname in
+      let ppm_chan = open_in ppmname in 
       print_endline ppmname;
       let w = ref "" in
-      let h = ref "" in 
+      let h = ref "" in
+      let matrix_decl = ref "" in 
       let ppm_lines = ref [] in
       try
         let ptype = input_line ppm_chan in
         let d = Str.split (Str.regexp " ") (input_line ppm_chan) in
         w := List.nth d 0;
         h := List.nth d 1;
+        matrix_decl := "[3][" ^ !w ^ ":" ^ !h  ^ "]";
         let max = input_line ppm_chan in
-        print_endline ppmname;
         while true; do
           ppm_lines := (clean_string (input_line ppm_chan)) :: !ppm_lines;
         done;
-        "(|" ^ (String.concat "|"
-        (List.map (fun i -> i) (List.rev !ppm_lines))) ^ "|" ^ ");"
-      with End_of_file -> ignore(close_in ppm_chan); "(|" ^ (String.concat "|"
-        (List.map (fun i -> i) (List.rev !ppm_lines))) ^ "|" ^  ");"
+        "int" ^ !matrix_decl ^ " " ^ id ^ (String.make 1 '\n') ^ ";" ^id ^ "= [|" ^ (String.concat "|"
+        (List.map (fun i -> i) (List.rev !ppm_lines))) ^ "|];"
+      with End_of_file -> ignore(close_in ppm_chan); "int" ^ !matrix_decl ^ " " ^ id
+      ^ ";" ^ (String.make 1 '\n') ^ id ^ "=[|" ^ (String.concat "|"
+      (List.map (fun i -> i) (List.rev !ppm_lines))) ^ "|];"
     in
-  let file_regex = Str.regexp "\".+\.ppm" in
-  let open_regex = Str.regexp "[0-9A-Za-z_]+[ ]*=[ ]*open\\(.+*\\)" in 
-    let has_open l = try ignore (Str.search_forward open_regex l 0); true
-      with Not_found -> false
-    in 
-    let has_file l =
-      if has_open l then 
-        try ignore(Str.search_forward file_regex l 0); true
+    let file_regex = Str.regexp "\".+\\.ppm" in
+    let decl_regex = Str.regexp "[0-9A-Za-z_]+[ ]*=[ ]*open\\(.+*\\);" in
+    let id_regex = Str.regexp "[0-9A-Za-z_]+[ ]*=" in
+    let has_decl l = try ignore(Str.search_forward decl_regex l 0); true
         with Not_found -> false
-      else false
-    in if (has_file l) then Str.replace_first open_regex (read_ppm (Str.string_after
-    (Str.matched_string l) 1)) l else l
+    in
+    let get_id l = try ignore(Str.search_forward id_regex l 0); 
+        Str.global_replace (Str.regexp "=") "" (Str.matched_string l)
+        with Not_found -> l
+    in
+    let get_file l = try ignore(Str.search_forward file_regex l 0);
+    (Str.string_after (Str.matched_string l) 1)
+        with Not_found -> l
+    in
+    let matrix_string l =
+      let decl_line = Str.matched_string l in
+      let ppm_file = get_file decl_line in
+      let id = get_id l in 
+      Str.replace_first decl_regex (read_ppm ppm_file id) l
+    in
+    let line= (if (has_decl l) then (matrix_string l) else l) in line
 in process_string (read_file filename)
