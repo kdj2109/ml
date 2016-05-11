c06a344e7ac2852d61e736a27701f2f9026c0842
kdj2109 <kdj2109@columbia.edu>
2016-05-11 11:45:17 -0400

    pointer test

---
+++ ml-new/ml-llvm/_build/ast.ml
@@ -0,0 +1,238 @@
+(* Abstract-syntax Tree for ML *)
+
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
+          And | Or
+
+type uop = Neg | Not
+
+type primitive = Int | Float | String | Bool | Char | Void
+
+type datatype =
+   TupleType of primitive * int
+ | MatrixType of datatype * int * int
+ | DataType of primitive
+ | TuplePointerType of primitive
+ | MatrixPointerType of primitive
+ | MatrixTuplePointerType of primitive
+
+type var_dec = datatype * string
+
+type expr =
+    IntLit of int
+  | CharLit of char
+  | FloatLit of float
+  | StrLit of string
+  | BoolLit of bool
+  | TupleAccess of string * expr
+  | MatrixAccess of string * expr * expr
+  | PointerIncrement of string
+  | TupleLiteral of expr list
+  | MatrixLiteral of expr list list
+  | Length of string
+  | Rows of string
+  | Columns of string
+  | Free of string
+  | TupleReference of string
+  | Dereference of string
+  | MatrixReference of string
+  | MatrixTupleReference of string
+  | Id of string
+  | Binop of expr * op * expr
+  | Unop of uop * expr
+  | Assign of expr * expr
+  | Call of string * expr list
+  | Noexpr
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | While of expr * stmt
+
+type func_decl = {
+    datatype : datatype;
+    fname : string;
+    formals : var_dec list;
+    locals : var_dec list;
+    body : stmt list;
+  }
+
+type program = var_dec list * func_decl list
+
+(* Pretty-printing functions *)
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+
+let string_of_tuple t =
+  let rec string_of_tuple_literal = function
+      [] -> "]"
+    | [hd] -> (match hd with
+                IntLit(i) -> string_of_int i
+              | FloatLit(f) -> string_of_float f
+              | StrLit(s) -> s
+              | CharLit(c) -> String.make 1 c
+              | BoolLit(true) -> "true"
+              | BoolLit(false) -> "false"
+              | Id(s) -> s
+              | _ -> raise( Failure("Illegal expression in tuple primitive") )) ^ string_of_tuple_literal []
+    | hd::tl -> (match hd with
+                    IntLit(i) -> string_of_int i ^ ", "
+                  | FloatLit(f) -> string_of_float f ^ ", "
+                  | StrLit(s) -> s ^ ", "
+                  | CharLit(c) -> (String.make 1 c) ^ ", "
+                  | BoolLit(true) -> "true" ^ ", "
+                  | BoolLit(false) -> "false" ^ ", "
+                  | Id(s) -> s
+                  | _ -> raise( Failure("Illegal expression in tuple primitive") )) ^ string_of_tuple_literal tl
+  in
+  "[" ^ string_of_tuple_literal t
+
+let string_of_matrix m r c =
+  let rec string_of_matrix_literal = function
+      [] -> "| " ^ string_of_int r ^ ", " ^ string_of_int c ^ "]"
+    | [hd] -> (match hd with
+                IntLit(i) -> string_of_int i
+              | FloatLit(f) -> string_of_float f
+              | StrLit(s) -> s
+              | CharLit(c) -> String.make 1 c
+              | BoolLit(true) -> "true"
+              | BoolLit(false) -> "false"
+              | Id(s) -> s
+              | TupleLiteral(t) -> string_of_tuple t
+              | _ -> raise( Failure("Illegal expression in matrix primitive") )) ^ string_of_matrix_literal []
+    | hd::tl -> (match hd with
+                    IntLit(i) -> string_of_int i ^ ", "
+                  | FloatLit(f) -> string_of_float f ^ ", "
+                  | StrLit(s) -> s ^ ", "
+                  | CharLit(c) -> (String.make 1 c) ^ ", "
+                  | BoolLit(true) -> "true" ^ ", "
+                  | BoolLit(false) -> "false" ^ ", "
+                  | Id(s) -> s
+                  | TupleLiteral(t) -> string_of_tuple t ^ ", "
+                  | _ -> raise( Failure("Illegal expression in matrix primitive") )) ^ string_of_matrix_literal tl
+  in
+  "[|" ^ string_of_matrix_literal m
+
+let rec string_of_expr = function
+    IntLit(i) -> string_of_int i
+  | FloatLit(f) -> string_of_float f
+  | StrLit(s) -> s
+  | CharLit(c) -> String.make 1 c
+  | BoolLit(true) -> "true"
+  | BoolLit(false) -> "false"
+  | Id(s) -> s
+  | TupleLiteral(t) -> string_of_tuple t
+  | MatrixLiteral(_) -> "matrix literal"
+  | TupleAccess(s, e) -> s ^ "[" ^ string_of_expr e ^ "]"
+  | MatrixAccess(s, e1, e2) -> s ^ "[" ^ string_of_expr e1 ^ ":" ^ string_of_expr e2 ^ "]"
+  | PointerIncrement(s) -> s ^ "++"
+  | Length(s) -> s ^ "." ^ "length"
+  | Rows(s) -> s ^ "." ^ "rows"
+  | Columns(s) -> s ^ "." ^ "columns"
+  | Free(s) -> "free" ^ "(" ^ s ^ ")"
+  | TupleReference(s) -> "@" ^ s
+  | Dereference(s) -> "$" ^ s
+  | MatrixReference(s) -> "@@" ^ s
+  | MatrixTupleReference(s) -> "@@@" ^ s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(e1, e2) -> string_of_expr e1 ^ " = " ^ string_of_expr e2
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    DataType(Int) -> "int"
+  | DataType(Float) -> "float"
+  | DataType(String) -> "str"
+  | DataType(Char) -> "char"
+  | DataType(Bool) -> "bool"
+  | DataType(Void) -> "void"
+  | TupleType(p, l) -> (match p with
+                          Int -> "int" ^ "[" ^ string_of_int l ^ "]"
+                        | Float -> "float" ^ "[" ^ string_of_int l ^ "]"
+                        | Char -> "char" ^ "[" ^ string_of_int l ^ "]"
+                        | String -> "str" ^ "[" ^ string_of_int l ^ "]"
+                        | Bool -> "bool" ^ "[" ^ string_of_int l ^ "]"
+                        | Void -> "void" ^ "[" ^ string_of_int l ^ "]")
+  | MatrixType(t, l1, l2) -> (match t with
+                                DataType(Int) -> "int" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | DataType(Float) -> "float" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | DataType(Char) -> "char" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | DataType(String) -> "str" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | DataType(Bool) -> "bool" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | DataType(Void) -> "void" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                              | TupleType(p, l) ->  (match p with
+                                                        Int -> "int" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                      | Float -> "float" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                      | Char -> "char" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                      | String -> "str" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                      | Bool -> "bool" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                      | Void -> "void" ^ "[" ^ string_of_int l ^ "]" ^ "[" ^ string_of_int l1 ^ ":" ^ string_of_int l2 ^ "]"
+                                                    )
+                              | _ -> raise ( Failure ("Illegal matrix of matrices") )
+                            )
+  | TuplePointerType(Int) -> "int[]"
+  | TuplePointerType(Float) -> "float[]"
+  | TuplePointerType(String) -> "str[]"
+  | TuplePointerType(Char) -> "char[]"
+  | TuplePointerType(Bool) -> "bool[]"
+  | TuplePointerType(Void) -> "void[]"
+  | MatrixPointerType(Int) -> "int[][]"
+  | MatrixPointerType(Float) -> "float[][]"
+  | MatrixPointerType(Char) -> "char[][]"
+  | MatrixPointerType(String) -> "str[][]"
+  | MatrixPointerType(Bool) -> "bool[][]"
+  | MatrixPointerType(Void) -> "void[][]"
+  | MatrixTuplePointerType(Int) -> "int[][][]"
+  | MatrixTuplePointerType(Float) -> "float[][][]"
+  | MatrixTuplePointerType(Char) -> "char[][][]"
+  | MatrixTuplePointerType(String) -> "str[][][]"
+  | MatrixTuplePointerType(Bool) -> "bool[][][]"
+  | MatrixTuplePointerType(Void) -> "void[][][]"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.datatype ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
---
+++ ml-new/ml-llvm/_build/parser.mly
@@ -0,0 +1,190 @@
+/* Ocamlyacc parser ML */
+
+%{ open Ast %}
+
+%token SEMI COLON LPAREN RPAREN LBRACE RBRACE LBRACK RBRACK BAR PERIOD COMMA
+%token PLUS MINUS TIMES DIVIDE ASSIGN AT DOLLAR
+%token EQ NEQ LT LEQ GT GEQ AND OR NOT
+%token RETURN IF ELSE ELSEIF FOR WHILE
+%token TRUE FALSE
+%token INT FLOAT BOOL CHAR VOID
+%token LENGTH ROWS COLUMNS FREE
+%token <int> INTLIT
+%token <float> FLOATLIT
+%token <char> CHARLIT
+%token <string> STRINGLIT
+%token <string> ID
+%token EOF
+
+%nonassoc NOELSE
+%nonassoc ELSE
+%right ASSIGN
+%left OR
+%left AND
+%left EQ NEQ
+%left LT GT LEQ GEQ
+%left PLUS MINUS
+%left TIMES DIVIDE
+%right NOT NEG
+
+%start program
+%type <Ast.program> program
+
+%%
+
+program:
+  decls EOF { (fst $1, snd $1) }
+
+decls:
+   /* nothing */ { [], [] }
+ | decls vdecl { ($2 :: fst $1), snd $1 }
+ | decls fdecl { fst $1, ($2 :: snd $1) }
+
+fdecl:
+   datatype ID LPAREN formals_opt RPAREN LBRACE vdecl_list stmt_list RBRACE
+     { { datatype = $1;
+         fname = $2;
+         formals = $4;
+         locals = List.rev $7;
+         body = List.rev $8 } }
+
+formals_opt:
+    /* nothing */ { [] }
+  | formal_list   { List.rev $1 }
+
+formal_list:
+    datatype ID                   { [($1, $2)] }
+  | formal_list COMMA datatype ID { ($3, $4) :: $1 }
+
+datatype:
+    primitive                 { DataType($1) }
+  | tuple_type                { $1 }
+  | matrix_type               { $1 }
+  | tuple_pointer_type        { $1 }
+  | matrix_pointer_type       { $1 }
+  | matrix_tuple_pointer_type { $1 }
+
+tuple_type:
+  primitive LBRACK INTLIT RBRACK { TupleType($1, $3) }
+
+matrix_type:
+    primitive LBRACK INTLIT COLON INTLIT RBRACK  { MatrixType(DataType($1), $3, $5) }
+  | tuple_type LBRACK INTLIT COLON INTLIT RBRACK { MatrixType($1, $3, $5) }
+
+tuple_pointer_type:
+  primitive LBRACK RBRACK { TuplePointerType($1) }
+
+matrix_pointer_type:
+  primitive LBRACK RBRACK LBRACK RBRACK { MatrixPointerType($1)}
+
+matrix_tuple_pointer_type:
+  primitive LBRACK RBRACK LBRACK RBRACK LBRACK RBRACK { MatrixTuplePointerType($1) }
+
+primitive:
+    INT    { Int }
+  | FLOAT  { Float }
+  | CHAR   { Char }
+  | BOOL   { Bool }
+  | VOID   { Void }
+
+vdecl_list:
+    /* nothing */    { [] }
+  | vdecl_list vdecl { $2 :: $1 }
+
+vdecl:
+  datatype ID SEMI { ($1, $2) }
+
+stmt_list:
+    /* nothing */  { [] }
+  | stmt_list stmt { $2 :: $1 }
+
+stmt:
+    expr SEMI { Expr $1 }
+  | RETURN SEMI { Return Noexpr }
+  | RETURN expr SEMI { Return $2 }
+  | LBRACE stmt_list RBRACE { Block(List.rev $2) }
+  | IF LPAREN expr RPAREN stmt %prec NOELSE { If($3, $5, Block([])) }
+  | IF LPAREN expr RPAREN stmt ELSE stmt    { If($3, $5, $7) }
+  | FOR LPAREN expr_opt SEMI expr SEMI expr_opt RPAREN stmt
+     { For($3, $5, $7, $9) }
+  | WHILE LPAREN expr RPAREN stmt { While($3, $5) }
+
+expr_opt:
+    /* nothing */ { Noexpr }
+  | expr          { $1 }
+
+expr:
+    literals                             { $1 }
+  | ID                                   { Id($1) }
+  | expr PLUS   expr                     { Binop($1, Add,   $3) }
+  | expr MINUS  expr                     { Binop($1, Sub,   $3) }
+  | expr TIMES  expr                     { Binop($1, Mult,  $3) }
+  | expr DIVIDE expr                     { Binop($1, Div,   $3) }
+  | expr EQ     expr                     { Binop($1, Equal, $3) }
+  | expr NEQ    expr                     { Binop($1, Neq,   $3) }
+  | expr LT     expr                     { Binop($1, Less,  $3) }
+  | expr LEQ    expr                     { Binop($1, Leq,   $3) }
+  | expr GT     expr                     { Binop($1, Greater, $3) }
+  | expr GEQ    expr                     { Binop($1, Geq,   $3) }
+  | expr AND    expr                     { Binop($1, And,   $3) }
+  | expr OR     expr                     { Binop($1, Or,    $3) }
+  | MINUS expr %prec NEG                 { Unop(Neg, $2) }
+  | NOT expr                             { Unop(Not, $2) }
+  | expr ASSIGN expr                     { Assign($1, $3) }
+  | ID LPAREN actuals_opt RPAREN         { Call($1, $3) }
+  | LPAREN expr RPAREN                   { $2 }
+  | ID LBRACK expr RBRACK                { TupleAccess($1, $3)}
+  | ID LBRACK expr COLON expr RBRACK     { MatrixAccess($1, $3, $5)}
+  | ID PERIOD LENGTH                     { Length($1) }
+  | ID PERIOD ROWS                       { Rows($1) }
+  | ID PERIOD COLUMNS                    { Columns($1) }
+  | AT ID                                { TupleReference($2) }
+  | DOLLAR ID                            { Dereference($2) }
+  | AT AT ID                             { MatrixReference($3)}
+  | AT AT AT ID                          { MatrixTupleReference($4) }
+  | ID PERIOD PLUS                       { PointerIncrement($1) }
+  | FREE LPAREN ID RPAREN                { Free($3) }
+
+primitives:
+    INTLIT    { IntLit($1) }
+  | FLOATLIT  { FloatLit($1) }
+  | STRINGLIT { StrLit($1) }
+  | TRUE      { BoolLit(true) }
+  | FALSE     { BoolLit(false) }
+  | CHARLIT   { CharLit($1) }
+
+literals:
+    primitives                                                   { $1 }
+  | LBRACK array_literal RBRACK                                  { TupleLiteral(List.rev $2) }
+  | LBRACK BAR multiple_matrix BAR RBRACK      { MatrixLiteral(List.rev $3) }
+  | LBRACK BAR tuple_multiple_matrix BAR RBRACK { MatrixLiteral(List.rev $3) }
+
+multiple_matrix:
+    | array_literal {[$1]}
+    | multiple_matrix BAR array_literal {$3 :: $1}
+
+tuple_multiple_matrix:
+    | tuple_literal_list {[$1]}
+    | tuple_multiple_matrix BAR tuple_literal_list {$3 :: $1}
+
+
+tuple_literal_list:
+    tuple_literal                          { [$1] }
+  | tuple_literal_list COMMA tuple_literal { $3 :: $1 }
+
+
+tuple_literal:
+  LPAREN array_literal RPAREN { TupleLiteral(List.rev $2) }
+
+
+array_literal:
+    literals                     { [$1] }
+  | array_literal COMMA literals { $3 :: $1 }
+
+actuals_opt:
+    /* nothing */ { [] }
+  | actuals_list  { List.rev $1 }
+
+actuals_list:
+    expr                    { [$1] }
+  | actuals_list COMMA expr { $3 :: $1 }
